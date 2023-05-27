c96e2da3b3be882fe0523fd8f9cf79953fb2e91f
andrew basem <andrew.basem@gmail.com>
2023-05-27 04:02:46 +0300

    restructure directory

--- statsbomb.session.sql
+++
@@ -1,8 +0,0 @@
-SELECT *,
-    away_team.team_name as away_team_name,
-    home_team.team_name as home_team_name
-FROM matches
-    JOIN teams as away_team
-        ON away_team.team_id = matches.away_team_id
-    JOIN teams as home_team
-        ON home_team.team_id = matches.home_team_id
